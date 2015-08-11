7fe608f2f073ce8cf7ab4578a4ec5d6252cb37f6
Adnan Begovic <adnan@cyngn.com>
2015-08-10 22:25:00 -0400

    Keyguard: Create lockscreen weather, move weatherimpl to utils.
    Change-Id: I1f78cb73fd320777d6151b8d00083b234a6b016e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -136,6 +136,7 @@ import com.android.internal.util.slim.ActionConfig;
 import com.android.internal.util.slim.ActionConstants;
 import com.android.internal.util.slim.ActionHelper;
 import com.android.internal.util.slim.DeviceUtils;
+import com.android.internal.util.slim.WeatherControllerImpl;
 
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.BatteryMeterView.BatteryMeterMode;
@@ -188,7 +189,6 @@ import com.android.systemui.statusbar.policy.SecurityControllerImpl;
 import com.android.systemui.statusbar.policy.SuControllerImpl;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
-import com.android.systemui.statusbar.policy.WeatherControllerImpl;
 import com.android.systemui.statusbar.policy.ZenModeController;
 import com.android.systemui.statusbar.stack.NotificationStackScrollLayout;
 import com.android.systemui.statusbar.stack.NotificationStackScrollLayout.OnChildLocationsChangedListener;
