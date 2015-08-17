9877063e510e6317f1ff0b1c7dea994780037db9
stofstik <verstegen.daan@gmail.com>
2015-08-17 17:42:53 +0200

    Added show volume panel tile to QS
    Change-Id: Ic583c24e304d1edc903127237fc9eb5c7eeb7e4d
    JIRA: CYAN-6721

--- core/java/com/android/internal/util/slim/QSConstants.java
+++ core/java/com/android/internal/util/slim/QSConstants.java
@@ -50,6 +50,7 @@ public class QSConstants {
     public static final String TILE_SLIMACTION = "slimaction";
     public static final String TILE_LIVE_DISPLAY = "live_display";
     public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
+    public static final String TILE_VOLUME = "volume_panel";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -90,5 +91,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_SLIMACTION);
         TILES_AVAILABLE.add(TILE_LIVE_DISPLAY);
         TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
+        TILES_AVAILABLE.add(TILE_VOLUME);
     }
 }
