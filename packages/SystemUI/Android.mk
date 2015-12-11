f1668813d4d577519ed1e026d39ce6ba8f2655aa
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-12-11 22:09:08 +0100

    QSTiles: Fix compile error

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -8,7 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
             Keyguard \
-            guava
+            guava \
+            android-support-v4
 
 LOCAL_JAVA_LIBRARIES := telephony-common
 
