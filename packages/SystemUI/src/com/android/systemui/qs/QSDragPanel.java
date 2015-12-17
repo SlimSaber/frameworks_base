fc8ec348167fe20bc2363ca64ded4f5c17b76787
Roman Birg <roman@cyngn.com>
2015-12-16 17:47:57 -0800

    SystemUI: fix incorrect tile shifting behavior
    We need to clamp the max row count to 3 so that the method which
    shifts any given tile to the next position can properly account when to
    put a tile on the next page.
    
    Also removed some extra debugging, make sure the user can't end up in a
    blank page after removing the last tile.
    
    Change-Id: I9367f35b1970c63383ab07dee2bf0622fcb8e94a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
@@ -60,6 +60,8 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
     private static final String TAG = "QSDragPanel";
 
     public static final boolean DEBUG_DRAG = false;
+
+    private static final int MAX_ROW_COUNT = 3;
     private static final String BROADCAST_TILE_SPEC_PLACEHOLDER = "broadcast_placeholder";
 
     protected final ArrayList<QSPage> mPages = new ArrayList<>();
@@ -192,7 +194,9 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
             public void onPageSelected(int position) {
                 if (mDragging && position != mDraggingRecord.page
                         && !mViewPager.isFakeDragging() && !mRestoring) {
-                    Log.w(TAG, "moving drag record to page: " + position);
+                    if (DEBUG_DRAG) {
+                        Log.w(TAG, "moving drag record to page: " + position);
+                    }
 
                     // remove it from the previous page and add it here
                     final QSPage sourceP = getPage(mDraggingRecord.page);
@@ -277,6 +281,7 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
         mQsPanelTop.onStopDrag();
 
         requestLayout();
+        ensurePagerState();
     }
 
     protected View getDropTarget() {
@@ -602,7 +607,7 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
         return cols;
     }
 
-    public int getMaxRows() {
+    public int getCurrentMaxRow() {
         int max = 0;
         for (TileRecord record : mRecords) {
             if (record.row > max) {
@@ -955,9 +960,10 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
             }
         }
 
-        int maxRows = getMaxRows();
+        // clamp this value to the max count we want.
+        int maxRows = Math.min(MAX_ROW_COUNT - 1 /* we are 0 based */, getCurrentMaxRow());
 
-        if (tile.row >= maxRows) {
+        if (tile.row > maxRows) {
             tile.destinationPage = tile.destinationPage + 1;
             tile.row = 0;
             tile.col = 0;
