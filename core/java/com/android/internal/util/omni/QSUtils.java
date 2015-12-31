0e3fcbc06099f988e9ba311f41de05b0df254eb7
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-12-31 16:33:42 +0100

    QSConstants: Clean up

--- core/java/com/android/internal/util/omni/QSUtils.java
+++ core/java/com/android/internal/util/omni/QSUtils.java
@@ -87,13 +87,7 @@ public class QSUtils {
             switch (tileKey) {
                 case QSConstants.TILE_CELLULAR:
                 case QSConstants.TILE_HOTSPOT:
-                case QSConstants.TILE_DATA:
-                case QSConstants.TILE_ROAMING:
-                case QSConstants.TILE_APN:
                     removeTile = !deviceSupportsMobile;
-                    break;
-                case QSConstants.TILE_DDS:
-                    removeTile = !deviceSupportsDdsSupported(context);
                     break;
                 case QSConstants.TILE_FLASHLIGHT:
                     removeTile = !deviceSupportsFlashLight(context);
@@ -207,13 +201,6 @@ public class QSUtils {
 
     public static void unregisterObserverForQSChanges(Context ctx, ContentObserver observer) {
         ctx.getContentResolver().unregisterContentObserver(observer);
-    }
-
-    public static boolean deviceSupportsDdsSupported(Context context) {
-        TelephonyManager tm = (TelephonyManager)
-                context.getSystemService(Context.TELEPHONY_SERVICE);
-        return tm.isMultiSimEnabled()
-                && tm.getMultiSimConfiguration() == TelephonyManager.MultiSimVariants.DSDA;
     }
 
     public static boolean deviceSupportsMobileData(Context ctx) {
